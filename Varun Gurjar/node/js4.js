aad4fae5dd1009eac65c900963f27246889dca42
varunbgurjar <varungurjar289@gmail.com>
2019-12-18 19:32:54 +0530

    logical/or/not/and

---
+++ Varun Gurjar/node/js4.js
@@ -0,0 +1,33 @@
+var x="100";
+var y="11";
+// this is logical not
+if(x%y==! (x/y))
+{
+    console.log("this is true ")
+}
+else (x/y)
+{
+    console.log("this is false ( logical not)")
+}
+var x="100";
+var y="10";
+//this is logical and
+if(x%y== 1 && (x/y))
+{
+   console.log("this is a true")
+}
+else
+{
+    console.log("this is false ( logical and)")
+}
+var x="100";
+var y="10";
+//this is logical or
+if(x%y==0 || (x/y))
+{
+   console.log("this is a true ( logical or)")
+}
+else
+{
+    console.log("this is false ")
+}
