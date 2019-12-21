8b49d0a820338e8dbfc99eda2736f3b9460365db
jamesjoel <james.joelj@gmail.com>
2019-12-21 12:24:28 +0530

    change password and shopping cart

--- james/ang1/src/app/app.module.ts
+++ james/ang1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { MiniDemoComponent } from './mini-demo/mini-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    MiniDemoComponent
   ],
   imports: [
     BrowserModule,
