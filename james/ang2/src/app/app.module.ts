8ed5703f27944429e8a17fcda0170cce341ad58d
jamesjoel <james.joelj@gmail.com>
2019-12-23 12:42:09 +0530

    routing and page level component

---
+++ james/ang2/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './page/about/about.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { HomeComponent } from './page/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
