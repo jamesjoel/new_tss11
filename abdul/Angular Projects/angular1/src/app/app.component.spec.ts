7395ec18f53c5f30b5702bd8b3638c9cfc2b9bd1
kaleem <kaleem@kalptaru.in>
2019-12-24 14:34:03 +0530

    new project

--- abdul/Angular Projects/angular1/src/app/app.component.spec.ts
+++ abdul/Angular Projects/angular1/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang1'`, () => {
+  it(`should have as title 'angular1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ang1');
+    expect(app.title).toEqual('angular1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ang1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular1 app is running!');
   });
 });
