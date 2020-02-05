17a856223a1fc18293e244ad3f1cbe008983e4f9
jamesjoel <james.joelj@gmail.com>
2020-02-05 12:36:50 +0530

    add and find in mongodb

--- node/day3/app.js
+++ node/day3/app.js
@@ -1,8 +1,57 @@
 var express = require("express");
 var app = express();
+var MongoClient = require("mongodb").MongoClient;
+var bodyParser = require("body-parser");
+
+
+// the MongoClient property has been create connection from NODE to MONGODB
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser());
+
+
+
+app.post("/save", (req, res)=>{
+    MongoClient.connect("mongodb://localhost:27017", (err, client)=>{
+        var db = client.db("tss11_30");
+        db.collection("teacher").insert(req.body, (err, result)=>{
+            res.redirect("/teacher");
+        })
+    });
+
+
+
+
+
+
+});
+
+
+app.get("/teacher", (req, res)=>{
+
+    // Create a Connect to Mongodb with "mongodb://localhost:27017" URL
+    MongoClient.connect("mongodb://localhost:27017", (err, client) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var db = client.db("tss11_30");
+
+        db.collection("teacher").find().toArray((err, result) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            // console.log(result);
+            res.render("layout", { pagename : "teacher", title : "Teacher", result : result});
+        });
+    });
+});
+
+
+
+
 
 app.get("/", (req, res)=>{
     res.render("layout", { pagename : "home", title : "Home"});
