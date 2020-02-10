05ddd5b5cb306ff4c662cf7ecd992c66844b8549
abdulishan <48240755+abdulishan@users.noreply.github.com>
2020-02-10 11:10:03 +0530

    jd

--- abdul/jd/controllers/home.js
+++ abdul/jd/controllers/home.js
@@ -2,7 +2,25 @@ var express = require("express");
 var routes = express.Router();
 
 routes.get("/", (req,res)=>{
-  res.render("layout", {title : "Home Page", pagename: "home/index" });
+  result = [
+    {
+    name : "restaurent",
+    img_url : "/imgs/sale2.jpg"
+    },
+    {
+      name : "shop online",
+      img_url : "/imgs/sale1.jpg"
+    },
+    {
+      name : "movies",
+      img_url : "/imgs/sale1.jpg"
+    },
+    {
+      name : "doctors",
+      img_url : "/imgs/sale2.jpg"
+    }
+]
+  res.render("layout", {title : "Home Page", pagename: "home/index", result : result });
 });
 
 module.exports = routes;
