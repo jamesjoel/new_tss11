73746a6662c5338503f2a78bc9d2c9bc1df24041
jamesjoel <james.joelj@gmail.com>
2020-03-11 13:15:30 +0530

    pagination day 2

--- finale-project/src/app/adv/pagination/pagination.component.ts
+++ finale-project/src/app/adv/pagination/pagination.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
@@ -9,10 +10,21 @@ export class PaginationComponent implements OnInit {
   arr=[];
   total = 0;
   totalPages = 0;
-  recordPerPage = 300;
-  constructor(private _http : HttpClient) { }
+  recordPerPage = 0;
+  constructor(private _http : HttpClient, private _actRoute : ActivatedRoute) { }
 
   ngOnInit() {
+    this._actRoute.queryParams.subscribe(params=>{
+      if(params.a){
+        this.recordPerPage = params.a;
+      }else{
+        
+        this.recordPerPage = 200;
+      }
+    })
+
+
+
     this._http.get<any>("http://localhost:3000/api/getcity/"+this.recordPerPage).subscribe(result=>{
       this.arr = result;
     });
